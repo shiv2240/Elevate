1d90713e5987dcc65b1057a1b1823e34ba50e86e
Shiv Sahni <shivsahni2240@gmail.com>
2025-05-27 22:52:50 +0530

    CORS enabled

--- Coding/PropFTX/Backend/server.js
+++ Coding/PropFTX/Backend/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const connect = require("./config/connect");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/movies", require("./routes/movieRoutes"));
 
