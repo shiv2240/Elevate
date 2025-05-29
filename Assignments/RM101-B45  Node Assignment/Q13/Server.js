86ef76802f65e755e5d8f1238c9ab3d0c7cf7ebe
Shiv Sahni <shivsahni2240@gmail.com>
2025-05-29 23:16:10 +0530

    Bugs fixed

--- Assignments/RM101-B45  Node Assignment/Q13/Server.js
+++ Assignments/RM101-B45  Node Assignment/Q13/Server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 5000
+const port = 5000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Home Route")
-}
-)
-//This bracke was missing
+  res.send("Home Route");
+});
+
 app.post("/data", (req, res) => {
-    const data = req.body;
-    res.send(`Received data: ${data}`);
-})
+  const {data} = req.body;
+  res.send(`Received data: ${data}`);
+});
 
 app.listen(port, () => {
-    console.log("Server running on port ${port}");
-})
-
+  console.log(`Server running on port ${port}`);
+});
